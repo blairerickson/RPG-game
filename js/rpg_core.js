95aaea6aec69450d95b9491056d125d0dca21743
Blair Erickson <blair@jamwix.com>
2017-02-17 02:13:57 -0500

    animation repair

--- js/rpg_core.js
+++ js/rpg_core.js
@@ -1,4 +1,14 @@
+//=============================================================================
+// rpg_core.js v1.3.5
+//=============================================================================
 
+//-----------------------------------------------------------------------------
+/**
+ * This is not a class, but contains some methods that will be added to the
+ * standard Javascript objects.
+ *
+ * @class JsExtensions
+ */
 function JsExtensions() {
     throw new Error('This is not a class');
 }
